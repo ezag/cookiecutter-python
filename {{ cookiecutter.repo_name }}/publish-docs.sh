15d34336fc8017cd2da33e8dfec92f0fe5218059
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2021-03-04 01:03:04 +0200

    Properly handle presence of wiki branch

--- {{ cookiecutter.repo_name }}/publish-docs.sh
+++ {{ cookiecutter.repo_name }}/publish-docs.sh
@@ -3,12 +3,17 @@ mkdir -p build/docs
 rm -rf build/docs/content
 git clone . build/docs/content
 cd build/docs/content
-git checkout --orphan wiki
+git remote add publish {{ cookiecutter.publish_url }}
+git fetch publish
+if git branch -a | grep remotes/publish/wiki; then
+    git checkout wiki
+else
+    git checkout --orphan wiki
+fi
 git rm -rf .
 cd -
 venv/bin/pydoc-markdown
 cd -
 git add .
 git commit -a -m "Auto commit - build-docs.sh"
-git remote add publish {{ cookiecutter.publish_url }}
 git push -u publish wiki
