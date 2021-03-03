69fef3768c196bcc8cf6dc91960989085663eb97
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2021-03-03 19:35:03 +0200

    python -> python3 (for alpine)

--- {{ cookiecutter.repo_name }}/reset-venv.sh
+++ {{ cookiecutter.repo_name }}/reset-venv.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/sh -ev
 rm -rf venv
-python -m venv venv
+python3 -m venv venv
 venv/bin/pip install --upgrade pip
 venv/bin/pip install -e .
 venv/bin/pip freeze | grep -v {{ cookiecutter.project_name }} > requirements.txt
