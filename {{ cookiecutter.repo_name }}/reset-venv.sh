168a816c210d638a0d65302cdcb315abfdba1129
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2021-03-03 23:09:16 +0200

    Add docs task

--- {{ cookiecutter.repo_name }}/reset-venv.sh
+++ {{ cookiecutter.repo_name }}/reset-venv.sh
@@ -1,21 +1,25 @@
-#!/bin/sh -ev
+#!/bin/bash -ev
 rm -rf venv
 python3 -m venv venv
 venv/bin/pip -q install --upgrade pip
 venv/bin/pip -q install -e .
 
-if [ "$1" != "ci" ]; then
+if [ \( "$1" != "tests" \) -a \( "$1" != "docs" \) ]; then
     set +e  # grep will return non-zero when requirements are empty
     venv/bin/pip freeze | grep -v {{ cookiecutter.project_name }} > requirements.txt
     set -e
 fi
 
-venv/bin/pip -q install -e .[ci]
+if [ "$1" == "tests" ]; then
+    venv/bin/pip -q install -e .[tests]
+fi
+if [ "$1" == "docs" ]; then
+    venv/bin/pip -q install -e .[docs]
+fi
 
-if [ "$1" != "ci" ]; then
-    venv/bin/pip freeze | grep -v {{ cookiecutter.project_name }} > requirements-ci.txt
-    venv/bin/pip -q install -e .[dev]
-    venv/bin/pip freeze | grep -v {{ cookiecutter.project_name }} > requirements-dev.txt
+if [ \( "$1" != "tests" \) -a \( "$1" != "docs" \) ]; then
+    venv/bin/pip -q install -e .[tests,docs,dev]
+    venv/bin/pip freeze | grep -v syaconf > requirements-dev.txt
     git add requirements*.txt
     git commit requirements*.txt -m "Auto freeze requirements - ./reset-venv.sh"
     venv/bin/pre-commit install
