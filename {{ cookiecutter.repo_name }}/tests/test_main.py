ecc3d1d4c20554b396f12d8801080661e141c4ed
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2021-03-03 18:28:11 +0200

    Capture & test std{out,err}

--- {{ cookiecutter.repo_name }}/tests/test_main.py
+++ {{ cookiecutter.repo_name }}/tests/test_main.py
@@ -1,7 +1,13 @@
 def test_PLEASE_DELETE_ME():
     assert False, "Just checking in and making sure I'm failing LOUD enough"
 
-def test_hello_world():
+def test_exit_code():
     from {{ cookiecutter.project_name }} import main
     exit_code = main()
-    assert exit_code == 0
+    assert exit_code is None
+
+def test_greeting(capsys):
+    # https://docs.pytest.org/en/stable/capture.html
+    captured = capsys.readouterr()
+    assert captured.out == "Hello World\n"
+    assert captured.err == ""
