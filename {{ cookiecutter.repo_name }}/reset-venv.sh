957e9576e0d9a0eea14ee5f63b629ddf1f3e082b
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2021-03-03 18:42:16 +0200

    Factor out fixture and add link to pytest docs

--- {{ cookiecutter.repo_name }}/reset-venv.sh
+++ {{ cookiecutter.repo_name }}/reset-venv.sh
@@ -4,6 +4,9 @@ python -m venv venv
 venv/bin/pip install --upgrade pip
 venv/bin/pip install -e .
 venv/bin/pip freeze | grep -v {{ cookiecutter.project_name }} > requirements.txt
-# TODO: define as development/CI requirements in setup.cfg
+# TODO: define as CI requirements in setup.cfg
 venv/bin/pip install tox
 venv/bin/pip freeze | grep -v {{ cookiecutter.project_name }} > requirements-ci.txt
+# TODO: define as development requirements in setup.cfg
+venv/bin/pip install pytest  # TODO: black ipython ipdb
+venv/bin/pip freeze | grep -v {{ cookiecutter.project_name }} > requirements-dev.txt
--- {{ cookiecutter.repo_name }}/tests/test_main.py
+++ {{ cookiecutter.repo_name }}/tests/test_main.py
@@ -1,12 +1,23 @@
+from pytest import fixture, mark
+
+
+@mark.xfail
 def test_PLEASE_DELETE_ME():
     assert False, "Just checking in and making sure I'm failing LOUD enough"
 
-def test_exit_code():
-    from {{ cookiecutter.project_name }} import main
-    exit_code = main()
-    assert exit_code is None
 
-def test_greeting(capsys):
+# https://docs.pytest.org/en/stable/reference.html?highlight=fixture#pytest-fixture
+@fixture
+def run_main():
+    from {{ cookiecutter.project_name }} import main
+    return main()
+
+
+def test_exit_code(run_main):
+    assert run_main is None  # exit code
+
+
+def test_greeting(capsys, run_main):
     # https://docs.pytest.org/en/stable/capture.html
     captured = capsys.readouterr()
     assert captured.out == "Hello World\n"
