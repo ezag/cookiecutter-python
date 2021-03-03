53e4d5ea21f5a5a3a039ae34664d1dbb89f0099e
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2021-03-03 17:14:11 +0200

    Links to setuptools_scm docs

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,3 +1,7 @@
 from setuptools import setup
 
-setup(use_scm_version=dict(root=".."))
+setup(
+    # https://github.com/pypa/setuptools_scm/tree/v5.0.1#setupcfg-usage
+    # https://github.com/pypa/setuptools_scm/tree/v5.0.1#configuration-parameters
+    use_scm_version=True,
+)
