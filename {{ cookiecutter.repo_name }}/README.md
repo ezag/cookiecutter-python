caf077e9d498a4c9f8ab64740e0145ffcada1009
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2021-03-03 17:41:23 +0200

    tox-quickstart

--- {{ cookiecutter.repo_name }}/README.md
+++ {{ cookiecutter.repo_name }}/README.md
@@ -14,4 +14,5 @@ git commit -m "Initial commit"
 git push -u origin main -f
 ./reset-venv.sh
 venv/bin/syaconf
+venv/bin/tox
 ```
--- {{ cookiecutter.repo_name }}/reset-venv.sh
+++ {{ cookiecutter.repo_name }}/reset-venv.sh
@@ -4,3 +4,6 @@ python -m venv venv
 venv/bin/pip install --upgrade pip
 venv/bin/pip install -e .
 venv/bin/pip freeze | grep -v {{ cookiecutter.project_name }} > requirements.txt
+# TODO: define as development/CI requirements in setup.cfg
+venv/bin/pip install tox
+venv/bin/pip freeze | grep -v {{ cookiecutter.project_name }} > requirements-ci.txt
