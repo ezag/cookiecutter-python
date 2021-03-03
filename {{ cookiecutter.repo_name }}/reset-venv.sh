cea439784cc3b7782bb270533d8fc7fa85999e0d
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2021-03-03 19:43:22 +0200

    Quiet pip, don't abort on empty requirements

--- {{ cookiecutter.repo_name }}/reset-venv.sh
+++ {{ cookiecutter.repo_name }}/reset-venv.sh
@@ -1,12 +1,14 @@
 #!/bin/sh -ev
 rm -rf venv
 python3 -m venv venv
-venv/bin/pip install --upgrade pip
-venv/bin/pip install -e .
+venv/bin/pip -q install --upgrade pip
+venv/bin/pip -q install -e .
+set +e
 venv/bin/pip freeze | grep -v {{ cookiecutter.project_name }} > requirements.txt
+set -e
 # TODO: define as CI requirements in setup.cfg
-venv/bin/pip install tox
+venv/bin/pip -q install tox
 venv/bin/pip freeze | grep -v {{ cookiecutter.project_name }} > requirements-ci.txt
 # TODO: define as development requirements in setup.cfg
-venv/bin/pip install pytest  # TODO: black ipython ipdb
+venv/bin/pip -q install pytest  # TODO: black ipython ipdb
 venv/bin/pip freeze | grep -v {{ cookiecutter.project_name }} > requirements-dev.txt
